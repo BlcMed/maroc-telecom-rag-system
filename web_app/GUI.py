074a18ab81dff6fe1985a9339b1e19167012cd3c
BlcMed <boulaich.mohamed970@gmail.com>
2024-08-14 10:21:02 +0100

    feat: initialize the GUI for the chatbot using streamlit framework

---
+++ web_app/GUI.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+TITLE="Maroc Telecom assistant"
+ASSISTANT_MESSAGE = "Message IAM Assistance"
+
+def response_generator(prompt):
+    response = f"response for {prompt} ..."
+    return response
+
+
+# GUI logic #
+
+st.title(TITLE)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input(ASSISTANT_MESSAGE)
+if prompt :
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    response = response_generator(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
